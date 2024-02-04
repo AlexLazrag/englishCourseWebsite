4694c321f1cc448e37cff91532231ff22258d233
AlexLazrag <alex.lazrag@gmail.com>
2024-02-04 12:43:40 +0200

    Email and phoneNumber annotations.

--- src/main/java/com/allaz/englishCourse/entity/user/User.java
+++ src/main/java/com/allaz/englishCourse/entity/user/User.java
@@ -1,8 +1,7 @@
 package com.allaz.englishCourse.entity.user;
 
 import jakarta.persistence.*;
-import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.*;
 import lombok.*;
 
 @Entity
@@ -27,9 +26,15 @@ public class User {
     @Size(min = 2, max = 255)
     private String lastName;
 
+    @Column(name = "email", unique = true, nullable = false)
+    @NotBlank(message = "Enter a valid email")
+    @Email
     private String email;
 
+    @Column(name = "phone_number", nullable = false)
+    @Pattern(regexp = "^(\\+|00)[0-9\\-]{10,15}$")
     private String phoneNumber;
+
 
     private String password;
 }
